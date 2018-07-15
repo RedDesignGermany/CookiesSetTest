fcd28480f991dd40267054bb40b2f469e4034190
Red Design Germany <6954004+RedDesignGermany@users.noreply.github.com>
2018-07-15 16:12:27 +0200

    Create cookie-test.php

---
+++ cookie-test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<--! Set a cookie for the TEST, otherwise disable the script -->
+<?php
+setcookie("test_cookie", "test", time() + 3600, '/');
+?>
+<html>
+<body>
+
+<?php
+if(count($_COOKIE) > 0) {
+     echo "Cookies are enabled.";
+} else {
+     echo "Cookies are disabled.";
+}
+?>
+
+</body>
+</html>
